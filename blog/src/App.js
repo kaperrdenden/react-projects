5a1b053f8bc25522c5e59137998f20a9a0948d6d
kaperrdenden <weather2sogood@gmail.com>
2022-03-03 12:47:10 +0900

    모달창 기능 상세구현

--- blog/src/App.js
+++ blog/src/App.js
@@ -9,7 +9,7 @@ function App() {
   let [글제목, 글제목변경] = useState(["남자 코트 추천", "강남 우동 맛집", "영화 추천"]);
   let [따봉, 따봉변경] = useState(0);
   let [modal, modal변경] = useState(false);
-
+  let [누른제목, 누른제목변경] = useState(0);
 
   function 제목바꾸기(){
     let coppiedArray = [...글제목];
@@ -34,45 +34,31 @@ function App() {
           <div>개발 blog</div>
 
         </div>
-        <button onClick={ 제목바꾸기 }> 버튼 </button>
-        <div className="list">
-         <h4> {글제목[0]}
-          <span onClick={()=>{  따봉변경(따봉 + 1) } }>👍</span>
-           {따봉} 
-           </h4>
-         <p>2월 17일 발행</p>
-          <hr/>
-        </div>
-        <div className="list">
-         <h4> {글제목[1]} </h4>
-         <p>2월 17일 발행</p>
-          <hr/>
-        </div>
-        <div className="list">
-         <h4 onClick={()=>{modal변경(!modal)}}> {글제목[2]} </h4>
-         <p>2월 17일 발행</p>
-          <hr/>
-        </div>
-      {
-        반복된UI()
-      }
+  
         {
-          글제목.map(function(글){
-            return  (<div className="list">
-              <h4> {글} 
-              <span onClick={()=>{  따봉변경(따봉 + 1) } }>👍</span>
-              </h4>
-              <p>2월 17일 발행</p>
-                dsadasd
+          글제목.map(function(글, i){
+            return (
+             <div className="list">
+                <h4 onClick={ ()=>{ 누른제목변경(i) } }> {글} 
+                  <span onClick={()=>{  따봉변경(따봉 + 1) } }>👍</span>
+                </h4>
+              
+                <p>2월 17일 발행</p>
+                
               <hr/>
-           </div>)
+           </div>
+           )
            
           })
         }
 
+        <button onClick={ ()=> {modal변경(!modal) }}>열고닫기</button>
+        <button onClick={ ()=>{ 누른제목변경(0) } }>버튼1</button>
+        <button onClick={ ()=>{ 누른제목변경(1) } }>버튼2</button>
+        <button onClick={  ()=>{ 누른제목변경(2) } }>버튼3</button>
         {
           modal === true
-          ? <Modal/>
+          ? <Modal 글제목={글제목} 누른제목={누른제목} />
           : null
         }
     
@@ -81,10 +67,10 @@ function App() {
   );
 }
 
-function Modal(){
+function Modal(props){
   return(
   <div className='modal'>
-    <h2>제목</h2>
+    <h2>{props.글제목[props.누른제목]}</h2>
     <p>날짜</p>
     <p>상세내용</p>
   </div>
