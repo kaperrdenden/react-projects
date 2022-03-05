1815589409111f50590beeaa735278c84bbc8bd6
kaperrdenden <weather2sogood@gmail.com>
2022-03-06 01:47:50 +0900

    좀 더 간결하게 .find를 이용하기

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -7,13 +7,10 @@ function Detail(props ){
     let sellectedShoe = null;
     let i=0;
     // let uniqueId = null;
-    for(i; i<props.shoes.length; i++){
-        console.log(props.shoes[i].id, id);
-        if(String(props.shoes[i].id) === id) {
-            sellectedShoe = props.shoes[i];
-        }
-    }
-    console.log(sellectedShoe);
+    let 찾은상품 = props.shoes.find(function(상품){
+        return 상품.id == id
+      });
+    // console.log(sellectedShoe);
     return(
       <div className="container">
       <div className="row">
@@ -21,9 +18,9 @@ function Detail(props ){
           <img src={"https://codingapple1.github.io/shop/shoes" + id + ".jpg" } width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{sellectedShoe.title}</h4>
-          <p>{sellectedShoe.content}</p>
-          <p>{sellectedShoe.price}</p>
+          <h4 className="pt-5">{찾은상품.title}</h4>
+          <p>{찾은상품.content}</p>
+          <p>{찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button> 
           <button className="btn btn-danger" onClick={()=>{
               history.push('/asdasdasd');
