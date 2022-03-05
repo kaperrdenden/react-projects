6683829df420bed1972b937392d4e95844e00526
kaperrdenden <weather2sogood@gmail.com>
2022-03-06 03:48:06 +0900

    색상을 변수에 저장하는 sass 문법

--- shop/src/Detail.js
+++ shop/src/Detail.js
@@ -1,14 +1,14 @@
 import React, {useState} from 'react';
 import { useHistory, useParams } from 'react-router-dom';
 import styled from 'styled-components';
-
+import './Detail.scss';
 
 let 박스 = styled.div`
     padding: 20px
 `;
 let 제목 = styled.h4`
     font-size: 25px; 
-    color: ${ props => props.색상 }
+    // color: ${ props => props.색상 }
 `;
 
 
@@ -27,14 +27,14 @@ function Detail(props ){
       <div className="container">
        
       <박스>
-        <제목 색상={"red"}>상세페이지</제목>
+        <제목 className='red' >상세페이지</제목>
       </박스>
       <div className="row">
         <div className="col-md-6">
           <img src={"https://codingapple1.github.io/shop/shoes" + id + ".jpg" } width="100%" />
         </div>
         <div className="col-md-6 mt-4">
-          <h4 className="pt-5">{찾은상품.title}</h4>
+          <h4 className="pt-5 red">{찾은상품.title}</h4>
           <p>{찾은상품.content}</p>
           <p>{찾은상품.price}</p>
           <button className="btn btn-danger">주문하기</button> 
