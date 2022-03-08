deb2c1953c1ec04279ecbdc9710cbaba141dcec4
kaperrdenden <weather2sogood@gmail.com>
2022-03-09 04:01:49 +0900

    redux 기본 사용

--- shop/src/Cart.js
+++ shop/src/Cart.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import {Table}  from 'react-bootstrap';
-
-function Cart(){
+import { connect } from 'react-redux';
+function Cart(props){
     return (
      
             <div>
@@ -18,7 +18,7 @@ function Cart(){
     <tbody>
       <tr>
         <td>1</td>
-        <td>Table cell</td>
+        <td>{props.state[0].name}</td>
         <td>Table cell</td>
         <td>Table cell</td>
         <td>Table cell</td>
@@ -28,11 +28,19 @@ function Cart(){
       
     </tbody>
   </Table>
-
-
 </div>
-        
     )
 }
 
-export default Cart;
+function 함수명(state){
+    // 이 함수는 redux store의 값들을 가져와서 props로 변환해주는 함수이다.
+    // 즉 state를 props화 시켜준다.
+    return {
+        state: state,
+        // redux store의 state값 중 name이라는 값을 가져와서 상품명에 담는다
+
+    }
+}
+export default connect(함수명)(Cart)
+
+// export default Cart;
