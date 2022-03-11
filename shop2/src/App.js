48a28f92ccb696ea20e6b7a6443a62e0e116bed6
kaperrdenden <weather2sogood@gmail.com>
2022-03-11 17:22:16 +0900

    login form 만들기

--- shop2/src/App.js
+++ shop2/src/App.js
@@ -1,12 +1,81 @@
 import logo from './logo.svg';
 import './App.css';
+import { useState } from 'react';
 
 function App() {
   return (
     <div className="App">
-    
+      <Loginform />
     </div>
   );
+}
+
+function Loginform(){
+
+  let [logindata, setLogindata] = useState({
+    id:'',
+    password:'',
+  });
+
+  return(
+
+    <>
+      <form onSubmit={(e)=>{
+        e.preventDefault();
+      }}>
+        <div className='input-form'>
+          <input
+            placeholder='id'
+            value={logindata.id}
+            onChange={(e)=>{
+              setLogindata(
+                {
+                  ...logindata,
+                  id:e.target.value,
+                }
+                )
+                console.log(logindata);
+            }
+           
+          }
+            
+            />
+          <input 
+            placeholder='password'
+            value={logindata.password}
+            onChange={(e)=>{
+              setLogindata(
+                {
+                  ...logindata,
+                  password:e.target.value,
+                }
+                )
+                console.log(logindata);
+            }
+           
+          }
+          />
+        </div>
+        <button 
+          className='Login-futton'
+          onClick={()=>{
+            if (logindata.id.length > 6 && logindata.password.length > 6){
+              console.log("corret");
+            }
+            setLogindata(
+              {
+                id:'',
+                password:'',
+              }
+            )
+          }}
+          >
+          로그인
+        </button>
+      </form>
+
+    </>
+  )
 }
 
 export default App;
