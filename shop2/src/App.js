7dd9df3afb10cc8b2052310e7279d3884f588403
kaperrdenden <weather2sogood@gmail.com>
2022-03-12 23:27:53 +0900

    Nav.js로 옮기기

--- shop2/src/App.js
+++ shop2/src/App.js
@@ -2,6 +2,8 @@ import logo from './logo.svg';
 import './App.css';
 import { useState } from 'react';
 import Loginform from './Loginform';
+
+
 function App() {
   return (
     <div className="App">
@@ -9,18 +11,7 @@ function App() {
     </div>
   );
 }
-function Nav(){
-  return(
-    <>
-      <div>
-        <div className='column'>home</div>
-        <div className='column'>detail</div>
-        <div className='column'>뒤로가기</div>
-        <div className='column'>더보기</div>
-      </div>
-    </>
-  )
-}
+
 
 
 export default App;
---
+++ shop2/src/Nav.js
@@ -0,0 +1,12 @@
+function Nav(){
+    return(
+      <>
+        <div>
+          <div className='column'>home</div>
+          <div className='column'>detail</div>
+          <div className='column'>뒤로가기</div>
+          <div className='column'>더보기</div>
+        </div>
+      </>
+    )
+  }
