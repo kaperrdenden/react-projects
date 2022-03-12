90a04619e85d4aa9b8401ba90c6f3a8a2c3e35ac
kaperrdenden <weather2sogood@gmail.com>
2022-03-12 23:26:50 +0900

    더보기

--- shop2/src/App.js
+++ shop2/src/App.js
@@ -16,6 +16,7 @@ function Nav(){
         <div className='column'>home</div>
         <div className='column'>detail</div>
         <div className='column'>뒤로가기</div>
+        <div className='column'>더보기</div>
       </div>
     </>
   )
