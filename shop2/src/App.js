a75332d3b1f7aec290a47da3f0d8a4162f439cbf
kaperrdenden <weather2sogood@gmail.com>
2022-03-13 05:51:41 +0900

    Shoesdata.js 작성

--- shop2/src/App.js
+++ shop2/src/App.js
@@ -4,8 +4,13 @@ import { useState } from 'react';
 import Loginform from './Loginform';
 import Nav from './Nav';
 import { Route,Link,Swtitch } from 'react-router-dom';
+import Shoesdata from './Shoesdata';
 
 function App() {
+  let [shoes, setShoes] = useState(Shoesdata);
+  
+  console.log(shoes);
+
   return (
     <div className="App">
       
